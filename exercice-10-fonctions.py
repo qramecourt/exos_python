8b4bdb733ac62dd57b6a4239be7c794f5567bd7a
Quentin Ramecourt <pro.quentin.ramecourt@gmail.com>
2022-04-08 16:46:59 +0200

    finition de l'exo 10

--- exercice-10-fonctions.py
+++ exercice-10-fonctions.py
@@ -33,7 +33,12 @@ def my_diff(a, b):
 # Appelez la fonction avec la valeur False et affichez le résultat
 
 # réponse 10.3
-
+def oui_non():
+    bool = True
+    if bool == True:
+        return 'oui'
+    else:
+        return 'non'
 # exo 10.4
 # Créer une fonction nommée `is_greater()` qui :
 # - prend deux paramètres `a` et `b` de type `float`
@@ -42,7 +47,13 @@ def my_diff(a, b):
 # Appelez la fonction et affichez le résultat
 
 # réponse 10.4
-
+def is_greater():
+    a = 1
+    b = 2
+    if (a > b): 
+        return True
+    else: 
+        return False
 # exo 10.5
 # Créer une fonction nommée `compare()` qui :
 # - prend deux paramètres `a` et `b` de type `float`
@@ -52,6 +63,17 @@ def my_diff(a, b):
 # Appelez la fonction et affichez le résultat
 
 # réponse 10.5
+def compare():
+    a = 1.5
+    b = 1.6
+    if (a > b):
+        return 1
+    elif(a < b):
+        return -1
+    else:
+        return 0
+
+print(compare())
 
 # exo 10.6
 # La formule suivante permet de convertir des mètres en miles :
@@ -75,7 +97,16 @@ def my_diff(a, b):
 # Appelez les fonctions et affichez les résultats
 
 # réponse 10.6
+def miles_to_meters():
+    a= 10
+    return a * 1609.344
 
+def meters_to_miles():
+    a = 1
+    return a / 1609.344
+
+print(meters_to_miles)
+print(miles_to_meters)
 # exo 10.7
 # Créer une fonction nommée `compute_tax()` qui :
 # - prend un paramètre nommé `price` de type `float`
@@ -90,3 +121,19 @@ def my_diff(a, b):
 # Référence : [Quels sont les taux de TVA en vigueur en France et dans l'Union européenne ? | economie.gouv.fr](https://www.economie.gouv.fr/cedef/taux-tva-france-et-union-europeenne)
 
 # réponse 10.7
+def compute_tax(price, tax_type):
+    if (tax_type == 1):
+        return 1.021 * price
+    elif (tax_type == 2):
+        return 1.055 * price
+    elif (tax_type == 3):
+        return 1.1 * price
+    else :
+        return 1.2 * price
+
+
+tax1 = compute_tax(100, 1)
+tax2 = compute_tax(100, 2)
+tax3 = compute_tax(100, 3)
+tax4 = compute_tax(100, 4)
+print(tax1, tax2, tax3, tax4)
