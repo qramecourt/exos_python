6b62c0fdfbab239d09c291a9fc9821a6e2ec3d66
Quentin Ramecourt <pro.quentin.ramecourt@gmail.com>
2022-04-08 14:34:19 +0200

    fin de l'exo 1

--- exercice-01-hello.py
+++ exercice-01-hello.py
@@ -2,11 +2,11 @@
 
 # exo 1.1
 # Affichez le message "programme démarré" en utilisant des doubles quotes
-
+print("programme démarré")
 # réponse 1.1
 
 # exo 1.2
 # Affichez le message "programme terminé" en utilisant des simples quotes
-
+print('programme fini')
 # réponse 1.2
 
