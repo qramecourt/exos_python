27d270953a5880e16f64399c9f3e85fe99cd2eee
Quentin Ramecourt <pro.quentin.ramecourt@gmail.com>
2022-04-11 17:29:58 +0200

    création du fichier d'exercices sur les classes

---
+++ 12-classes.py
@@ -0,0 +1,19 @@
+# une class nommée Foo avec des variables d'instance :
+# - bar
+# - baz
+# - lorem
+# et des méthodes getters et setters
+class Foo:
+    def __init__(self, bar, baz):
+        self.bar = bar
+        self.baz = baz
+
+
+    def get_bar(self):
+        return self.bar
+    def ger_baz(self):
+        return self.baz
+    def set_bae
+
+# import de classes depuis d'autres fichiers
+# normalement, les imports se font au tout début des scripts
